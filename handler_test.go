b10a90b657d8deb23289a869c85cc0bdcb2f3e7a
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-26 15:19:45 +0700

    Add function TestServeMux

--- handler_test.go
+++ handler_test.go
@@ -19,3 +19,22 @@ func TestHandler(t *testing.T) {
 	err := server.ListenAndServe()
 	errHandler(err)
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hellow World!!")
+	})
+
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Testing Page")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	errHandler(err)
+}
