d4a4d93d792da33340e63b4f53107a707d445d2c
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-26 17:17:53 +0700

    Add file Response code test

---
+++ response_code_test.go
@@ -0,0 +1,35 @@
+package goweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func ResponseCode(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+
+	if name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "name is empty")
+	} else {
+		fmt.Fprintf(w, "Hello %s", name)
+	}
+}
+
+func TestResponseCodeInvalid(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:"+port, nil)
+	rec := httptest.NewRecorder()
+
+	ResponseCode(rec, req)
+
+	res := rec.Result()
+	body, err := io.ReadAll(res.Body)
+	errHandler(err)
+
+	fmt.Println(res.StatusCode)
+	fmt.Println(res.Status)
+	fmt.Println(body)
+}
