d208188b24323b094061a6e9868c28f252d71bab
Arif Rachman Hakim <arifrachman149@gmail.com>
2022-12-27 18:06:03 +0700

    Update template html test

--- template_html_test.go
+++ template_html_test.go
@@ -19,11 +19,27 @@ func SimpleHTML(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "Simple", "Test Template HTML on Go-Lang")
 }
 
+func SimpleHTMLFile(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/simple.gohtml"))
+	t.ExecuteTemplate(w, "simple.gohtml", "Test Template")
+}
+
 func TestSimpleHTML(t *testing.T) {
-	req := httptest.NewRequest(http.MethodGet, "http://localhost:", port)
+	req := httptest.NewRequest(http.MethodGet, "http://localhost:"+port, nil)
 	rec := httptest.NewRecorder()
 
 	SimpleHTML(rec, req)
+
+	body, err := io.ReadAll(rec.Result().Body)
+	errHandler(err)
+	fmt.Println(string(body))
+}
+
+func TestSimpleHTMLFile(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "http://localhost:"+port, nil)
+	rec := httptest.NewRecorder()
+
+	SimpleHTMLFile(rec, req)
 
 	body, err := io.ReadAll(rec.Result().Body)
 	errHandler(err)
